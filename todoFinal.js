7212879383b7b457ce72b5dc11dc1af1da6bed9d
chriia01 <chriia01@luther.edu>
2015-10-19 14:23:17 -0500

    Update todoFinal.js

--- todoFinal.js
+++ todoFinal.js
@@ -1,6 +1,9 @@
 addList = function() {
-  var newLi, prioritySelector;
+  var newLi, inputBox, prioritySelector;
+  
+  inputBox = document.querySelector(#toDoItem)
   newLi = document.creatElement("li");
+  newLi.createTextNode(inputBox.value)
   prioritySelector = document.querySelector(#priority);
   newLi.className = priority.value;
 }
